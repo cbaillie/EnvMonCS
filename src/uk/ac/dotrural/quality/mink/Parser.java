6dfb91318e19f72435f3b5444bb965c22cdc0985
chris <c.baillie@abdn.ac.uk>
2013-06-20 13:41:49 +0100

    Initial commit.

---
+++ src/uk/ac/dotrural/quality/mink/Parser.java
@@ -0,0 +1,15 @@
+package uk.ac.dotrural.quality.mink;
+
+public class Parser {
+	
+	public static void main(String[] args)
+	{
+		new Parser();
+	}
+	
+	public Parser()
+	{
+		System.out.println("Hello World!");
+	}
+
+}
